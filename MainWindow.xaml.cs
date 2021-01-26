ab0fdeb5fbd83cb7055a3f392cad348a2a48161f
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2021-01-26 09:17:15 +0100

    oupsy

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -80,7 +80,7 @@ namespace LBDCUpdater
             if (conflicts > 0)
             {
                 infoControl.Mode = InstallationInfo.DisplayMode.WARNING;
-                infoControl.Text = missing == 1 ? $"{missing} mod est en trop. Cliquez pour choisir l'action à réaliser." : $"{missing} mods sont en trop. Cliquez pour choisir l'action à réaliser.";
+                infoControl.Text = conflicts == 1 ? $"{conflicts} mod est en trop. Cliquez pour choisir l'action à réaliser." : $"{missing} mods sont en trop. Cliquez pour choisir l'action à réaliser.";
                 infoControl.Click = null; //TODO lier à la blacklist
                 infoControl.Clickable = true;
                 return;
